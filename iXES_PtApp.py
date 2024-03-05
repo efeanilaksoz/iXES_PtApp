74e8c419bb3e208fd7df71b9c3c3caab55483f11
Anil <97220727+efeanilaksoz@users.noreply.github.com>
2024-03-05 13:32:47 +0100

    Update iXES_PtApp.py

--- iXES_PtApp.py
+++ iXES_PtApp.py
@@ -4,6 +4,7 @@ import dash_daq as daq
 #import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, use_pages=True)
+server = app.server
 
 if __name__ == '__main__':
-	app.run_server(debug=True, host='0.0.0.0', port=8050)
+	app.run_server(debug=False)
